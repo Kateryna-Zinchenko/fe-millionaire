0f28e8e6df3e3831effeef4239b78224fa84589e
Kateryna Zinchenko <zinchenko.kate220@gmail.com>
2023-08-07 19:01:49 +0300

    add: GitHub pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-	alias: {
-	  '@': '/src'
-	}
-  },
+export default defineConfig(({command}) => {
+  const config = {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
+    },
+    base: '/'
+  }
+  if (command !== 'serve') {
+    config.base = '/fe-millionaire/'
+  }
+
+  return config
 });
